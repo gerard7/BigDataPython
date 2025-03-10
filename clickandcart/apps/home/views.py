e4b4ace945926bd2610a92dd85f7e10c9ee2f9ee
Armand <gerardarm@yahoo.fr>
2025-03-10 10:25:00 +0100

    Création de la première page home_view et sa vue

---
+++ clickandcart/apps/home/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request):
+    return HttpResponse("Bienvenue sur la première page d'accueil !")
