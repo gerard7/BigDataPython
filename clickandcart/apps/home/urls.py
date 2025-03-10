6e244cd2c9aedfee508e4e7e8ac48454daf4fd68
Armand <gerardarm@yahoo.fr>
2025-03-10 10:22:43 +0100

    Création de la première page home_view et sa vue

---
+++ clickandcart/apps/home/urls.py
@@ -0,0 +1,9 @@
+from apps.home.views import home_view
+
+from django.urls import  path
+
+urlpatterns =[
+    path("",home_view,name="home")  # "", c'est l'url : ici / suffit pour l'appeler . Ensuite , c'est le nom de la méthode name="home" c'est le nom de la route
+
+
+]
